e35284cfb12e584dc232b6375450d01b670f46f6
Gautam Bhandarkar <34414643+GautamAtGitHub@users.noreply.github.com>
2018-04-26 18:19:03 +0530

    JWT Bearer Authentication

--- WebAPI_Security/Controllers/ValuesController.cs
+++ WebAPI_Security/Controllers/ValuesController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace WebAPI_Security.Controllers
 {
@@ -11,6 +15,7 @@ namespace WebAPI_Security.Controllers
     {
         // GET api/values
         [HttpGet]
+        [Authorize]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
@@ -39,6 +44,36 @@ namespace WebAPI_Security.Controllers
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+        }
+
+
+        ///Method to generate Sample tocken. This is sample tocken generator method
+        ///In case of production environment tocken can be generated from forms authentication 
+        ///or from secure tocken server
+        ///or from OAuth server
+        ///
+        [Route("GetToken")]
+        [HttpGet]
+        public IActionResult GetToken()
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, "jeremy@jeremylikness.com"),
+                new Claim(JwtRegisteredClaimNames.Jti, System.Guid.NewGuid().ToString()),
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(Startup.SecretKEY_AES256));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken("http://localhost:5001",
+                "http://localhost:5000",
+                claims,
+                expires: System.DateTime.Now.AddMinutes(30),
+                signingCredentials: creds);
+
+            var tokenEncoded = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return new OkObjectResult(new { token = tokenEncoded });
         }
     }
 }
--- WebAPI_Security/Startup.cs
+++ WebAPI_Security/Startup.cs
@@ -8,11 +8,15 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace WebAPI_Security
 {
     public class Startup
     {
+        //This is sample secret, in production you can get this value from DB, config or from secure server.
+        public const string SecretKEY_AES256 = @"0123456789ABCDEF";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -24,6 +28,29 @@ namespace WebAPI_Security
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddAuthentication(options => {
+                options.DefaultAuthenticateScheme = "JwtBearer";
+                options.DefaultChallengeScheme = "JwtBearer";
+            })
+            .AddJwtBearer("JwtBearer", jwtBearerOptions =>
+            {
+                jwtBearerOptions.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true, // verify signature to avoid tampering
+                    IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(SecretKEY_AES256)),
+
+                    ValidateIssuer = true,
+                    ValidIssuer = "http://localhost:5001", // site that makes the token
+
+                    ValidateAudience = true,
+                    ValidAudience = "http://localhost:5000", // site that consumes the token
+
+                    ValidateLifetime = true, //validate the expiration and not before values in the token
+
+                    ClockSkew = System.TimeSpan.FromMinutes(5) //5 minute tolerance for the expiration date
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +61,7 @@ namespace WebAPI_Security
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseAuthentication();
             app.UseMvc();
         }
     }
